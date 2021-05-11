611413637406603ad9833039d66c356563a41006
nhat khoa <nhatkhoa.tin@gmail.com>
2021-05-11 16:44:56 +0700

    update post method

--- routes/candidates.js
+++ routes/candidates.js
@@ -1,33 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+fs.readFile(`${__dirname}/candidates.json`, 'utf8', (err , data) => {
+    if (err) throw err;
+    candidatesData =  JSON.parse(data)
+
+})
+
+function save(data){
+    const json = JSON.stringify(data);
+    fs.writeFile("./routes/candidates.json", json, function(err, data) {
+        if(err) return console.log(err)
+    })
+}
 
 
-let candidatesData =  require('./candidates.json');
 
 /* GET candidates listing. */
 router.get('/', function(req, res, next) {
-    if(req.query){
-        if(req.query.ethnicity){
-            let candidateByEthnicity = candidatesData.filter(c => c.ethnicity === req.query.ethnicity);
-            if(req.query.company){
-                let candidateByEthnicityAndCompany =  candidateByEthnicity.filter(c => c.company === req.query.company);
-                res.json(candidateByEthnicityAndCompany);
-            } else {
-                res.json(candidateByEthnicity);
-            }
-        } else {
-            if(req.query.company){
-                let candidateByCompany =  candidatesData.filter(c => c.company === req.query.company);
-                res.json(candidateByCompany);
-            } else {
-                res.json(candidatesData)
-            }
-        }
+    // if(req.query){
+    //     if(req.query.ethnicity){
+    //         let candidateByEthnicity = candidatesData.filter(c => c.ethnicity === req.query.ethnicity);
+    //         if(req.query.company){
+    //             let candidateByEthnicityAndCompany =  candidateByEthnicity.filter(c => c.company === req.query.company);
+    //             res.json(candidateByEthnicityAndCompany);
+    //         } else {
+    //             res.json(candidateByEthnicity);
+    //         }
+    //     } else {
+    //         if(req.query.company){
+    //             let candidateByCompany =  candidatesData.filter(c => c.company === req.query.company);
+    //             res.json(candidateByCompany);
+    //         } else {
+    //             res.json(candidatesData)
+    //         }
+    //     }
 
-    } else {
-        res.json(candidatesData)
-    }
-
+    // } else {
+    //     res.json(candidatesData)
+    // }
+    let candidates = candidatesData;
+    const queryStringFilter = Object.keys(req.query);
+    console.log({queryStringFilter})
+    if(queryStringFilter.length !== 0 ){
+        queryStringFilter.map(filter => {
+            candidates = candidates.filter(candidate => candidate[filter] === req.query[filter])
+        })
+    };
+    
+    res.json(candidates);
 });
 
 
@@ -37,9 +59,13 @@ router.get('/register', function(req, res, next) {
 
 // register post methods
 
-router.post('/register', function(req, res, next) {
-    console.log(req);
-    res.send('<h1>Register POST method</h1>')
+router.post('/', function(req, res) {
+   let candidates = candidatesData;
+    const candidate = req.body;
+    candidate.id = candidates.length + 1;
+    candidates.push(candidate);
+    save(candidates); 
+    res.send(candidate);
 });
 
 
